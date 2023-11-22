ae7057eda7160c71b59181532ab0b9ffc79f0e28
Saptarshi Majumdar <saptarshi.maj@yahoo.com>
2023-11-22 19:51:24 +0530

    working on add review to bootcamp

--- server.js
+++ server.js
@@ -1,14 +1,66 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const reviews = require("./routes/reviews");
+
 const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// * Cookie Parser
+app.use(cookieParser());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// File uploading
+app.use(fileupload());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
+app.use("/api/v1/reviews", reviews);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
 );
+
+// Handle Unhandled Promise Rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exitprocess
+  server.close(() => process.exit(1));
+});
